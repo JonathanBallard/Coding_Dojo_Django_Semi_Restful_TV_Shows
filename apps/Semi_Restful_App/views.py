2c7e5bc08e0317cf969a295a5f18bc2addb88402
Jonathan Ballard <asgardschosen@gmail.com>
2020-04-19 16:48:24 -0700

    finished routes

--- apps/Semi_Restful_App/views.py
+++ apps/Semi_Restful_App/views.py
@@ -14,24 +14,41 @@ def shows(request):
     return render(request, 'Semi_Restful_App/index.html', context) 
 
 def newShow(request):
-    return render(request, 'Semi_Restful_app/new_show.html')
+    return render(request, 'Semi_Restful_App/new_show.html')
 
 def createShow(request):
     Show.objects.create(title=request.POST['title'], network=request.POST['network'], release_date=request.POST['release_date'], desc = request.POST['description'])
 
-    return redirect('shows/')
+    return redirect('/shows/')
 
 def thisShow(request, id):
-    pass
+    context = {
+        "this_show" : Show.objects.get(id=id),
+    }
+    return render(request, 'Semi_Restful_App/this_show.html',context)
 
 def editThisShow(request,id):
-    pass
+    context = {
+        "this_show" : Show.objects.get(id=id),
+    }
+    return render(request, 'Semi_Restful_App/edit_show.html', context)
 
 def updateThisShow(request,id):
-    pass
+
+    updateShow = Show.objects.get(id=id)
+    updateShow.title=request.POST['title']
+    updateShow.network=request.POST['network']
+    updateShow.release_date=request.POST['release_date']
+    updateShow.desc = request.POST['description']
+
+    updateShow.save()
+
+    return redirect('/shows/')
 
 def destroyThisShow(request,id):
-    pass
+    delShow = Show.objects.get(id=id)
+    delShow.delete()
+    return redirect('/shows/')
 
 
 
