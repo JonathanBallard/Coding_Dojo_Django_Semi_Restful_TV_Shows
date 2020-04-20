f47a8c74e84254a2c5d35b8dd632502c29442872
Jonathan Ballard <asgardschosen@gmail.com>
2020-04-19 18:30:14 -0700

    starting to add manager

--- apps/Semi_Restful_App/models.py
+++ apps/Semi_Restful_App/models.py
@@ -10,3 +10,17 @@ class Show(models.Model):
     desc = models.TextField(default="Description...")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+class ShowManager(models.Manager):
+    def validator(self, postData):
+        errors = {}
+        if len(postData['title']) < 2:
+            errors['title'] = "Title must be at least 2 characters"
+        if len(postData['network']) < 3:
+            errors['network'] = "Network must be at least 3 characters"
+        if len(postData['desc']) < 10 and len(postData['desc']) > 0:
+            errors['desc'] = "Description is optional, but must be at least 10 characters if used"
+        
+        
+
+        return errors
